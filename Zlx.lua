87b6204fe8ec78f0dfa35cb5f288de8604a10295
Dannyboi21 <danieldjr2000@gmail.com>
2026-03-25 17:35:51 -0700

    Create Zlx.lua

---
+++ Zlx.lua
@@ -0,0 +1,6 @@
+local Players = game:GetService("Players")
+
+local player = Players:GetPlayerFromCharacter(script.Parent)
+if player then
+	player:Kick("You have been reported to the roblox HQ due to continues of exploiting/Permantly banned")
+end
